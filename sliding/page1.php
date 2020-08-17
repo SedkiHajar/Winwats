3e46a0cd942727bb356ff78034e5cab09cc6770e
HajarSEDKI <hajar_sedki@um5.ac.ma>
2020-08-17 01:01:15 +0100

    done

--- sliding/page1.php
+++ sliding/page1.php
@@ -5,7 +5,9 @@
 	<meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Acme&display=swap" rel="stylesheet">
-    <title>WINWATS</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Product Image Magnifier</title>
+    <link rel="stylesheet" href="src/jquery.exzoom.css">
     <link rel="stylesheet" href="css/styles.css" />
      
     <link
@@ -42,25 +44,60 @@ $result = $db->query("SELECT * FROM produit where titre=$nom ");
     <div class="container p-5">
     <div class="row">
         <?php while($row = $result->fetch_assoc()){ ?> 
-        	<div class="col-lg-6 mb-3 mb-lg-0">
-            <img width="50%" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>"  /> 
-             
-           <?php echo '<br>' .$row['titre'] ?>
+        	<div class="exzoom" id="exzoom">
+              <div class="exzoom_img_box">
+                 <ul class='exzoom_img_ul'>
+                    <li><img width="500px" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['image']); ?>"  />
+                    </li>
+                 </ul>
+              </div>
+              <div class="exzoom_nav"></div> 
+            </div>
+
+
+            <div class="trending" style="margin-left: 120px;">  
+           <h1><?php echo '<br>' .$row['titre'] ?></h1>
             
-            <?php echo '<br>' .$row['description'] ?>
+            <h2><?php echo '<br>' .$row['description'] ?></h2>
             
             <?php  } ?> 
+            </div>
 
-          </div>
+          
     
-</div> 
-</div> 
-</div> 
+    </div> 
+    </div> 
+    </div> 
 <?php }else{ ?> 
     <p class="status error">Image(s) not found...</p> 
 <?php } }?>  
 
+ </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script src="src/jquery.exzoom.js"></script>
 
+    <script>
+        $(function(){
+
+            $("#exzoom").exzoom({
+
+            // thumbnail nav options
+            "navWidth": 60,
+            "navHeight": 60,
+            "navItemNum": 5,
+            "navItemMargin": 7,
+            "navBorder": 1,
+
+            // autoplay
+            "autoPlay": false,
+
+            // autoplay interval in milliseconds
+            "autoPlayTimeout": 2000
+            
+            });
+
+        });
+    </script>
 
 
 </body>
